c5c78bece8ef8ca0f1a2582ba11e4bb9febee94f
luckybhatti28 <luckybhatti271@gmail.com>
2021-09-27 12:33:58 -0700

    resume-on-react.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <>
+    <h1 class='heading Experience'>Lucky Bhatti</h1>;
+    <div class='headingp'>
+    <p> Luckybhatti271@gamil.com   /  03040115906  /  Punjab Colony, Karachi , Sindh</p>
+    </div>
+    <h4 class='h5heading summaryheading Experience'>Summary</h4>
+    <p class='Summaryp'> 
+    Knowledgeable Web Developer brings superior front- and back-end design to promote organization-specific website
+presence. Thorough comprehension of complex HTML, CSS and JavaScript programming languages to generate
+custom webpage design. Extensive collaboration with programming to ascertain company expectations and oversee
+site creation from initial planning through rollout and support. Detail-oriented approach to maintaining website
+responsiveness, effectiveness and security. Media-focused Web Developer integrates imagery, video and sound to
+provide innovative sites and apps. Skilled at balancing usability with aesthetic appeal. Attracts users with interactive
+assets and individualized experiences.
+</p>
+<hr class='hrwidth'></hr>
+<h4 class='h5heading  Experience '>Experience</h4>
+<h5 class='text-center h6heading'>Front End Developer</h5>
+<h6 class='text-center Ipt'>Fiverr</h6>
+<h5 class='text-center width-foo' >Prepared customized client presentations and quotes</h5>
+<hr class='hrwidth'></hr>
+<h4 class='h5heading  Experience'>Education</h4>
+<h6 class='text-center Ipt'>Trinity private school • Karachi, Sindh</h6>
+<h5 class='text-center width-foo'>Currently completing courses in matric</h5>
+<hr class='hrwidth'></hr>
+<h4 class='h5heading  Experience'>Skills</h4>
+<ul class='skilllang'>
+  <li>HTML5</li>
+  <li>CSS</li>
+  <li>Bootstrap</li>
+  <li>JavaScript</li>
+</ul>
+<hr class='hrwidth'></hr>
+<h4 class='h5heading  Experience It'>Languages</h4>
+<ul class='skilllang'>
+  <li>English</li>
+  <li>Urdu</li>
+</ul>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  </>
+  ,document.getElementById('root')
+)
